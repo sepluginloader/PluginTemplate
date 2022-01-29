e7467b9fc357c27029a64abeede0ca3c75b971df
WesternGamer <80211714+WesternGamer@users.noreply.github.com>
2022-01-29 16:26:24 -0500

    Log System change. (#1)
    * Removed NLog system from ClientPlugin and DedicatedPlugin and replaced with new logging system.
    
    * Added exception handler on Init method.
    
    * Removed temp code.
    
    * Updated README with info about building project.
    
    * Reverted batch file.

--- ClientPlugin/Plugin.cs
+++ ClientPlugin/Plugin.cs
@@ -1,7 +1,8 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using HarmonyLib;
-using NLog;
 using VRage.Plugins;
+using VRage.Utils;
 
 namespace ClientPlugin
 {
@@ -10,7 +11,7 @@ namespace ClientPlugin
     {
         public const string Name = "PluginTemplate";
 
-        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        private static readonly PluginLogger Log = new PluginLogger(Name);
 
         private static bool initialized;
 
@@ -19,14 +20,29 @@ namespace ClientPlugin
         [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
         public void Init(object gameInstance)
         {
-            Log.Debug($"{Name}: Patching");
-            Harmony.PatchAll(Assembly.GetExecutingAssembly());
-            Log.Info($"{Name}: Patches applied");
+            Log.WriteLine(MyLogSeverity.Debug, "Patching");            
+            try
+            {
+                Harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Log.WriteException(MyLogSeverity.Critical, ex);
+            }
+            Log.WriteLine(MyLogSeverity.Info, "Patches applied");
         }
 
         public void Dispose()
         {
-            // Do NOT use harmony.UnpatchAll()
+            try
+            {
+                // TODO: Put anything that needs to be disposed/closed when the game closes here
+                // Do NOT use harmony.UnpatchAll()
+            }
+            catch (Exception ex)
+            {
+                Log.WriteException(MyLogSeverity.Critical, ex);
+            }
         }
 
         public void Update()
@@ -37,15 +53,31 @@ namespace ClientPlugin
             Initialize();
 
             initialized = true;
+
+            try
+            {
+                // TODO: Put anything that needs to be called on update here
+            }
+            catch (Exception ex)
+            {
+                Log.WriteException(MyLogSeverity.Critical, ex);
+            }
         }
 
         private void Initialize()
         {
-            Log.Debug($"{Name}: Initializing");
+            Log.WriteLine(MyLogSeverity.Info, "Initializing");
 
-            // TODO: Put your one time initialization here
+            try
+            {
+                // TODO: Put your one time initialization here
+            }
+            catch (Exception ex)
+            {
+                Log.WriteException(MyLogSeverity.Critical, ex);
+            }
 
-            Log.Info($"{Name}: Initialized");
+            Log.WriteLine(MyLogSeverity.Info, "Initialized");
         }
     }
 }

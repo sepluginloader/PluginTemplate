685b1aa68eea635d80672d924133a2a3b297b9d0
Viktor Ferenczi <viktor@ferenczi.eu>
2022-02-05 00:30:47 +0100

    Error handling improvements, optional use of Harmony in TorchPlugin, fixed assembly name (#12)

--- ClientPlugin/Properties/AssemblyInfo.cs
+++ ClientPlugin/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClientPlugin")]
+[assembly: AssemblyTitle("PluginTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClientPlugin")]
+[assembly: AssemblyProduct("PluginTemplate")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
